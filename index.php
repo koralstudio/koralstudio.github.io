5b555a67bd7a9aed764438b36bdadb6f79e702b7
Joey van Ommen <joey@messagebird.com>
2017-03-18 03:16:59 +0100

    stuff

--- index.php
+++ index.php
@@ -20,8 +20,10 @@
 
         <!-- Add your site or application content here -->
         <p>Hello world! This is HTML5 Boilerplate.</p>
-        <script src="https://code.jquery.com/jquery-3.2.0.min.js" integrity="sha256-JAW99MJVpJBGcbzEuXk4Az05s/XyDdBomFqNlM3ic+I=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.0.min.js"><\/script>')</script>
+
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/jquery/dist/jquery.fullpage.min.js"></script>
+
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
 
@@ -33,4 +35,3 @@
         <script src="https://www.google-analytics.com/analytics.js" async defer></script>
     </body>
 </html>
-0
